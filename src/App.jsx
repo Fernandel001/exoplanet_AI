ffdfd9374f4ff6a47983887c7e8988a984c07959
Fernandel001 <werindoissireal@gmail.com>
2025-10-04 18:57:25 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Dashboard from './Components/Dashboard'
+import About from './Components/About'
+
+function App() {
+
+  return (
+    <div>
+      <About/>
+    </div>
+  )
+}
+
+export default App
